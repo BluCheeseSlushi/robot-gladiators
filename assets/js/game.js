d36ade882a0fbe731610a9bb4281dc973e994e5e
Josiah Douglass <josiahdouglass64@gmail.com>
2022-02-18 15:13:07 -0500

    updated name prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,16 @@
+// function to set name
+let getPlayerName = function() {
+    let name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+}
+
 let playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 25,
     money: 10,
